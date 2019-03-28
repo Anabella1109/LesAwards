2106dcb58dc6499e7f7d34f32a3dc10bff230a59
TUYISENGE Anabella <bellaxbx1109@gmail.com>
2019-03-28 15:42:42 +0200

    add Form to add profile in forms.py and views.py

--- lewawards/views.py
+++ lewawards/views.py
@@ -4,4 +4,37 @@ from django.http  import HttpResponse,Http404,HttpResponseRedirect
 def home(request):
     return render(request,'projects/home.html')
 
+@login_required(login_url='/accounts/login/')
+def profile(request,id):
+     user=User.objects.get(id=id)
+     profile=Profile.objects.get(user=user)
+    #  images=Image.objects.filter(user=user)
+    #  following1=following(user)
+    #  followers1=followers(profile)
+    
+     return render(request, 'grams/profile.html',{"user":user,"profile": profile})
+     
+@login_required(login_url='/accounts/login/')
+def edit_profile(request,edit):
+    current_user = request.user
+    profile=Profile.objects.get(user=current_user)
+    
+   
+    if request.method == 'POST':
+        form = Profileform(request.POST, request.FILES)
+        if form.is_valid():
+            
+            profile.bio=form.cleaned_data['bio']
+            profile.photo = form.cleaned_data['photo']
+            profile.user=current_user
+            
+            profile.save()
+        return redirect('home')
+
+    else:
+        form = Profileform()
+    return render(request, 'edit_profile.html', {"form": form , 'user':current_user})
+
+
+
 # Create your views here.
