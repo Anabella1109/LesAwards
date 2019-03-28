ba5f0191e40536039324721a7eca2032ed4d0fe5
TUYISENGE Anabella <bellaxbx1109@gmail.com>
2019-03-28 15:20:10 +0200

    add Profile model in models.py

--- lewawards/models.py
+++ lewawards/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
+
+class Profile(models.Model):
+    photo=models.ImageField(upload_to='images/',default='images/avatar.jpg')
+    bio=models.TextField()
+    user=models.OneToOneField(User,on_delete=models.CASCADE,null=True)
+    first_name=models.CharField(max_length=100)
+    last_name=models.CharField(max_length=100)
+    phone_number=models.IntegerField()
+
+
+@receiver(post_save, sender=User)
+def update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
 # Create your models here.
