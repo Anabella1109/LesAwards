cbc3bcf2676251133be8504beb50582820b41095
TUYISENGE Anabella <bellaxbx1109@gmail.com>
2019-03-28 15:01:18 +0200

    add bootsrap and add it in installed apps in settings

--- lewawards/urls.py
+++ lewawards/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns=[
    
     url('^$',views.home,name='home'),
-    url(r'^new/post$', views.new_post, name='new-post'),
+    # url(r'^new/post$', views.new_post, name='new-post'),
     # url(r'^post/(\d+)',views.post,name ='post') ,
     # url(r'^like_home/(\d+)',views.like_home,name ='like1') ,
     # url(r'^like_post/(\d+)',views.like_post,name ='like2') ,
@@ -21,5 +21,5 @@ urlpatterns=[
 
    
 ]
-if settings.DEBUG:
-    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- lewawards/views.py
+++ lewawards/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render, redirect
 from django.http  import HttpResponse,Http404,HttpResponseRedirect
 
 def home(request):
-    return render(request,'home.html')
+    return render(request,'projects/home.html')
 
 # Create your views here.
